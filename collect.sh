bc67573708ad7c3cce3f1a3209fcef17089b739c
c3eru <96016081+c3eru@users.noreply.github.com>
2021-12-30 09:24:41 +0700

    Update collect.sh

--- collect.sh
+++ collect.sh
@@ -2,7 +2,7 @@
 cd /tmp/rom
 
 . build/envsetup.sh
-lunch nad_juice-userdebug
+lunch spark_juice-userdebug
 curl -s https://api.telegram.org/$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d text="$(echo "${var_cache_report_config}")"
 export CCACHE_DIR=/tmp/ccache
 export CCACHE_EXEC=$(which ccache)
@@ -10,7 +10,7 @@ export USE_CCACHE=1
 ccache -M 20G
 ccache -o compression=true
 ccache -z
-make nad -j8 &
+mka bacon -j8 &
 sleep 90m
 kill %1
 ccache -s
