4d1a2282b24225f025cac1f2a8b1e6f949b7cb96
c3eru <96016081+c3eru@users.noreply.github.com>
2021-12-31 16:20:33 +0700

    Update collect.sh

--- collect.sh
+++ collect.sh
@@ -2,7 +2,7 @@
 cd /tmp/rom
 
 . build/envsetup.sh
-lunch aosp_juice-userdebug
+lunch weeb_juice-userdebug
 curl -s https://api.telegram.org/$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d text="$(echo "${var_cache_report_config}")"
 export CCACHE_DIR=/tmp/ccache
 export CCACHE_EXEC=$(which ccache)
@@ -10,7 +10,7 @@ export USE_CCACHE=1
 ccache -M 20G
 ccache -o compression=true
 ccache -z
-mka bacon -j8 &
+make weeb-prod -j$(nproc --all) &
 sleep 90m
 kill %1
 ccache -s
